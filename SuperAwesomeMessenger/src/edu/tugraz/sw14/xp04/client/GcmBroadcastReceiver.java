a0ffe19fbc8e993737699b585be83dc952018c5b
rreitbauer <rainer.reitbauer@student.tugraz.at>
2014-04-02 16:53:36 +0200

    [RR,MF]{issue} I03 - notification service

--- SuperAwesomeMessenger/src/edu/tugraz/sw14/xp04/client/GcmBroadcastReceiver.java
+++ SuperAwesomeMessenger/src/edu/tugraz/sw14/xp04/client/GcmBroadcastReceiver.java
@@ -26,12 +26,14 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
 					Log.d(TAG, "- " + cat);
 			}
 		}
-		/*
-		 * // Explicitly specify that GcmIntentService will handle the intent.
-		 * ComponentName comp = new ComponentName(context.getPackageName(),
-		 * GcmIntentService.class.getName()); // Start the service, keeping the
-		 * device awake while it is launching. startWakefulService(context,
-		 * (intent.setComponent(comp))); setResultCode(Activity.RESULT_OK);
-		 */
+
+		// Explicitly specify that GcmIntentService will handle the intent.
+		ComponentName comp = new ComponentName(context.getPackageName(),
+				GcmIntentService.class.getName()); // Start the service, keeping
+													// the
+		// device awake while it is launching.
+		startWakefulService(context, (intent.setComponent(comp)));
+		setResultCode(Activity.RESULT_OK);
+
 	}
 }
