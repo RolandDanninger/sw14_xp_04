c1f475d2ae3e4d3d0d2ed18125a2506e356ce062
knuke92 <oberrauner.peter@gmail.com>
2014-04-09 09:31:34 +0200

    [CH,PO]{issue} I08 - Added robotium framework to the Messenger and added RegistrationActivity Testclass.

---
+++ SuperAwesomeMessenger/test/edu/tugraz/sw14/xp04/ActivityRegistrationTest.java
@@ -0,0 +1,17 @@
+package edu.tugraz.sw14.xp04;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+public class ActivityRegistrationTest extends
+		ActivityInstrumentationTestCase2<ActivityRegistration> {
+
+	private Solo solo;
+
+	public ActivityRegistrationTest() {
+		super(ActivityRegistration.class);
+	}
+
+
+}
