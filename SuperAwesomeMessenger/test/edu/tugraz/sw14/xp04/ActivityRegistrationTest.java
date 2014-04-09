fd13b631f12766c302d52657caf29f2540d02617
knuke92 <oberrauner.peter@gmail.com>
2014-04-09 15:27:42 +0200

    [CH,PO]{issue} I08 - Implemented Registration logic.

--- SuperAwesomeMessenger/test/edu/tugraz/sw14/xp04/ActivityRegistrationTest.java
+++ SuperAwesomeMessenger/test/edu/tugraz/sw14/xp04/ActivityRegistrationTest.java
@@ -26,8 +26,10 @@ public class ActivityRegistrationTest extends
 
 	public void testRegistrationSuccessfull() {
 		EditText txtID = (EditText) solo.getView(R.id.a_registration_txt_id);
-		EditText txtPassword = (EditText) solo.getView(R.id.a_registration_txt_password);
-		EditText txtReenterPassword = (EditText) solo.getView(R.id.a_registration_txt_reenter_password);
+		EditText txtPassword = (EditText) solo
+				.getView(R.id.a_registration_txt_password);
+		EditText txtReenterPassword = (EditText) solo
+				.getView(R.id.a_registration_txt_reenter_password);
 
 		solo.enterText(txtID, "test@gmail.com");
 		solo.enterText(txtPassword, "1234");
@@ -35,13 +37,16 @@ public class ActivityRegistrationTest extends
 
 		solo.clickOnButton(solo.getString(R.string.a_registration_btn_register));
 
-		assertTrue(solo.waitForText("Registration successfull!"));
+		assertTrue(solo.waitForText(solo
+				.getString(R.string.a_registration_success)));
 	}
 
 	public void testRegistrationFailedIDMissing() {
 		EditText txtID = (EditText) solo.getView(R.id.a_registration_txt_id);
-		EditText txtPassword = (EditText) solo.getView(R.id.a_registration_txt_password);
-		EditText txtReenterPassword = (EditText) solo.getView(R.id.a_registration_txt_reenter_password);
+		EditText txtPassword = (EditText) solo
+				.getView(R.id.a_registration_txt_password);
+		EditText txtReenterPassword = (EditText) solo
+				.getView(R.id.a_registration_txt_reenter_password);
 
 		solo.enterText(txtID, "");
 		solo.enterText(txtPassword, "1234");
@@ -49,13 +54,16 @@ public class ActivityRegistrationTest extends
 
 		solo.clickOnButton(solo.getString(R.string.a_registration_btn_register));
 
-		assertTrue(solo.waitForText("Please enter an e-mail address!"));
+		assertTrue(solo.waitForText(solo
+				.getString(R.string.a_registration_error_missing_id)));
 	}
-	
+
 	public void testRegistrationFailedPasswordMissing() {
 		EditText txtID = (EditText) solo.getView(R.id.a_registration_txt_id);
-		EditText txtPassword = (EditText) solo.getView(R.id.a_registration_txt_password);
-		EditText txtReenterPassword = (EditText) solo.getView(R.id.a_registration_txt_reenter_password);
+		EditText txtPassword = (EditText) solo
+				.getView(R.id.a_registration_txt_password);
+		EditText txtReenterPassword = (EditText) solo
+				.getView(R.id.a_registration_txt_reenter_password);
 
 		solo.enterText(txtID, "test@gmail.com");
 		solo.enterText(txtPassword, "");
@@ -63,27 +71,16 @@ public class ActivityRegistrationTest extends
 
 		solo.clickOnButton(solo.getString(R.string.a_registration_btn_register));
 
-		assertTrue(solo.waitForText("Please enter a password!"));
+		assertTrue(solo.waitForText(solo
+				.getString(R.string.a_registration_error_missing_password)));
 	}
-	
-	public void testRegistrationFailedReenterMissing() {
+
+	public void testRegistrationFailedPasswordsMismatch() {
 		EditText txtID = (EditText) solo.getView(R.id.a_registration_txt_id);
-		EditText txtPassword = (EditText) solo.getView(R.id.a_registration_txt_password);
-		EditText txtReenterPassword = (EditText) solo.getView(R.id.a_registration_txt_reenter_password);
-
-		solo.enterText(txtID, "test@gmail.com");
-		solo.enterText(txtPassword, "1234");
-		solo.enterText(txtReenterPassword, "");
-
-		solo.clickOnButton(solo.getString(R.string.a_registration_btn_register));
-
-		assertTrue(solo.waitForText("Please re-enter your password!"));
-	}
-	
-	public void testRegistrationFailedPasswordsNotMatching() {
-		EditText txtID = (EditText) solo.getView(R.id.a_registration_txt_id);
-		EditText txtPassword = (EditText) solo.getView(R.id.a_registration_txt_password);
-		EditText txtReenterPassword = (EditText) solo.getView(R.id.a_registration_txt_reenter_password);
+		EditText txtPassword = (EditText) solo
+				.getView(R.id.a_registration_txt_password);
+		EditText txtReenterPassword = (EditText) solo
+				.getView(R.id.a_registration_txt_reenter_password);
 
 		solo.enterText(txtID, "test@gmail.com");
 		solo.enterText(txtPassword, "1234");
@@ -91,12 +88,16 @@ public class ActivityRegistrationTest extends
 
 		solo.clickOnButton(solo.getString(R.string.a_registration_btn_register));
 
-		assertTrue(solo.waitForText("Passwords do not match!"));
+		assertTrue(solo.waitForText(solo
+				.getString(R.string.a_registration_error_passwords_mismatch)));
 	}
+
 	public void testRegistrationFailedInvalidID() {
 		EditText txtID = (EditText) solo.getView(R.id.a_registration_txt_id);
-		EditText txtPassword = (EditText) solo.getView(R.id.a_registration_txt_password);
-		EditText txtReenterPassword = (EditText) solo.getView(R.id.a_registration_txt_reenter_password);
+		EditText txtPassword = (EditText) solo
+				.getView(R.id.a_registration_txt_password);
+		EditText txtReenterPassword = (EditText) solo
+				.getView(R.id.a_registration_txt_reenter_password);
 
 		solo.enterText(txtID, "test");
 		solo.enterText(txtPassword, "1234");
@@ -104,7 +105,8 @@ public class ActivityRegistrationTest extends
 
 		solo.clickOnButton(solo.getString(R.string.a_registration_btn_register));
 
-		assertTrue(solo.waitForText("Please enter a valid e-mail address!"));
+		assertTrue(solo.waitForText(solo
+				.getString(R.string.a_registration_error_invalid_id)));
 	}
 
-	}
+}
