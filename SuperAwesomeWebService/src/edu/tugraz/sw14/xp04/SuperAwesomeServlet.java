3ee738bad2a94669de748c4b06800ed685280e15
rreitbauer <rainer.reitbauer@student.tugraz.at>
2014-05-14 09:38:51 +0200

    [SM,RR]{issue} I16 - added sender to message response

--- SuperAwesomeWebService/src/edu/tugraz/sw14/xp04/SuperAwesomeServlet.java
+++ SuperAwesomeWebService/src/edu/tugraz/sw14/xp04/SuperAwesomeServlet.java
@@ -8,24 +8,15 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
+import javax.servlet.http.HttpSession;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -47,9 +38,10 @@ public class SuperAwesomeServlet extends HttpServlet {
 		this.jsonMapper = new ObjectMapper();
 	}
 
+	@Override
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		String action = (String) request.getParameter("action");
+		String action = request.getParameter("action");
 
 		if (action == null) {
 			return;
@@ -58,11 +50,9 @@ public class SuperAwesomeServlet extends HttpServlet {
 		try {
 			if (action.compareTo("login") == 0) {
 				login(request, response);
-			}
-			else if (action.compareTo("register") == 0) {
+			} else if (action.compareTo("register") == 0) {
 				register(request, response);
-			}
-			else if (action.compareTo("send") == 0) {
+			} else if (action.compareTo("send") == 0) {
 				sendMessage(request, response);
 			}
 		} catch (ServerException e) {
@@ -77,143 +67,144 @@ public class SuperAwesomeServlet extends HttpServlet {
 		LoginRequest req = null;
 
 		try {
-			req = jsonMapper.readValue(request.getInputStream(), LoginRequest.class);
+			req = jsonMapper.readValue(request.getInputStream(),
+					LoginRequest.class);
 		} catch (Exception e) {
 			throw new ServerException("Failed to parse LoginRequest.", e);
 		}
-		
+
 		ServletController controller = new ServletController();
 		LoginResponse res = controller.login(req);
-		
+
+		if (!res.isError()) {
+			HttpSession session = request.getSession(true);
+			session.setAttribute("id", req.getId());
+			session.setMaxInactiveInterval(60 * 60); // time in s(1h)
+		}
+
 		try {
 			jsonMapper.writeValue(response.getOutputStream(), res);
 		} catch (Exception e) {
-			throw new ServerException("Failed to parse LoginResponse to JSON.",	e);
+			throw new ServerException("Failed to parse LoginResponse to JSON.",
+					e);
 		}
 
 	}
-	
-	private void register(HttpServletRequest request, HttpServletResponse response)
-			throws ServerException, IOException, UserException {
+
+	private void register(HttpServletRequest request,
+			HttpServletResponse response) throws ServerException, IOException,
+			UserException {
 		RegistrationRequest req = null;
-		
+
 		try {
-			req = jsonMapper.readValue(request.getInputStream(), RegistrationRequest.class);
+			req = jsonMapper.readValue(request.getInputStream(),
+					RegistrationRequest.class);
 		} catch (Exception e) {
 			throw new ServerException("Failed to parse LoginRequest.", e);
 		}
-		
+
 		ServletController controller = new ServletController();
 		RegistrationResponse res = controller.register(req);
-		
+
 		try {
 			jsonMapper.writeValue(response.getOutputStream(), res);
 		} catch (Exception e) {
-			throw new ServerException("Failed to parse RegistrationResponse to JSON.",	e);
+			throw new ServerException(
+					"Failed to parse RegistrationResponse to JSON.", e);
 		}
 	}
-	
-	private void sendMessage(HttpServletRequest request, HttpServletResponse response) 
-			throws ServerException, IOException {
+
+	private void sendMessage(HttpServletRequest request,
+			HttpServletResponse response) throws ServerException, IOException {
 		ServletController controller = new ServletController();
 		SendMessageRequest req = null;
-		
+
 		try {
-			req = jsonMapper.readValue(request.getInputStream(), SendMessageRequest.class);
+			req = jsonMapper.readValue(request.getInputStream(),
+					SendMessageRequest.class);
 		} catch (Exception e) {
 			System.err.println("Failed to parse SendMessageRequest.");
 			throw new ServerException("Failed to parse SendMessageRequest.", e);
 		}
-		
-//		HttpClient client = new DefaultHttpClient();
-//
-//		HttpPost httpPost = new HttpPost("https://android.googleapis.com/gcm/send");
-//		httpPost.addHeader("Authorization", "key=AIzaSyDaEIpAaziLsL9SVA5N3_MzPdf8FLVA7wA");
-//		
+
 		URL url = null;
-		
+
 		try {
 			url = new URL("https://android.googleapis.com/gcm/send");
-		}
-		catch(MalformedURLException e) {
+		} catch (MalformedURLException e) {
 			System.err.println("invalid url sendMessage");
 			throw new ServerException("invalid url sendMessage");
 		}
-		
+
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			throw new ServerException("invalid session");
+		}
+
 		HttpURLConnection connection = null;
-		
+
 		try {
 			connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("POST");
 			connection.setRequestProperty("Content-Type", "application/json");
-			connection.setRequestProperty("Authorization", "key=AIzaSyDaEIpAaziLsL9SVA5N3_MzPdf8FLVA7wA");
+			connection.setRequestProperty("Authorization",
+					"key=AIzaSyDaEIpAaziLsL9SVA5N3_MzPdf8FLVA7wA");
 			connection.setUseCaches(false);
 			connection.setDoOutput(true);
-			
+
 			User receiver = controller.findUserByEmail(req.getReceiverId());
-			
+
 			GCMMessage msg = new GCMMessage();
 			msg.setRegistration_ids(Arrays.asList(receiver.getGcmId()));
-			
+
 			System.err.println(receiver.getGcmId());
-			
+
+			String id = (String) session.getAttribute("id");
 			Map<String, String> data = new HashMap<String, String>();
 			data.put("message", req.getMessage());
-			data.put("sender", "SENDER");
+			data.put("sender", id);
 			msg.setData(data);
-	
+
 			OutputStream out = connection.getOutputStream();
-			
+
 			String jsonGCMMsg = jsonMapper.writeValueAsString(msg);
-			
+
 			System.err.println(jsonGCMMsg);
-			
+
 			out.write(jsonGCMMsg.getBytes());
 			out.close();
-			
+
 			int status = connection.getResponseCode();
-	        if (status == 200) {
-	        	InputStream is = connection.getInputStream();
-	        	InputStreamReader isReader = new InputStreamReader(is);
-	        	BufferedReader bReader = new BufferedReader(isReader);
-	        	
-	        	String line = "";
-	        	while( (line = bReader.readLine()) != null) {
-	        		System.err.println(line);
-	        	}
-	        	
-	        	bReader.close();
-	        }
-	        else {
-	        	System.err.println("Post failed with error code ");
-	        	throw new IOException("Post failed with error code " + status);
-	        }
-		}
-		finally {
-			if(connection != null) {
+			if (status == 200) {
+				InputStream is = connection.getInputStream();
+				InputStreamReader isReader = new InputStreamReader(is);
+				BufferedReader bReader = new BufferedReader(isReader);
+
+				String line = "";
+				while ((line = bReader.readLine()) != null) {
+					System.err.println(line);
+				}
+
+				bReader.close();
+			} else {
+				System.err.println("Post failed with error code ");
+				throw new IOException("Post failed with error code " + status);
+			}
+		} finally {
+			if (connection != null) {
 				connection.disconnect();
 			}
 		}
-		
-//		StringEntity entity = new StringEntity(jsonGCMMsg);
-//		entity.setContentType("application/json");
-//		httpPost.setEntity(entity);
-//		
-//		HttpResponse gcmResponse = client.execute(httpPost);
-		
-		//GCM RESPONSE
-//		RegistrationRequest r = new RegistrationRequest();
-//		r = jsonMapper.readValue(gcmResponse.getEntity().getContent(), RegistrationRequest.class);
-		//
-		
+
 		SendMessageResponse res = new SendMessageResponse();
 		res.setError(false);
 		res.setErrorMessage(null);
-		
+
 		try {
 			jsonMapper.writeValue(response.getOutputStream(), res);
 		} catch (Exception e) {
-			throw new ServerException("Failed to parse RegistrationResponse to JSON.",	e);
+			throw new ServerException(
+					"Failed to parse RegistrationResponse to JSON.", e);
 		}
 	}
 }
