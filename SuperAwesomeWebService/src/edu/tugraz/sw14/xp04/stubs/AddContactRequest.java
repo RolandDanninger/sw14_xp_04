7cbd5fdaee31682c7ecf6c2c845e80f3d5727956
huca <carmen.huber@student.tugraz.at>
2014-05-21 11:20:33 +0200

    [CH,MF]{issue} I24 - added ContactStubs; created AddContactListener

---
+++ SuperAwesomeWebService/src/edu/tugraz/sw14/xp04/stubs/AddContactRequest.java
@@ -0,0 +1,13 @@
+package edu.tugraz.sw14.xp04.stubs;
+
+public class AddContactRequest{
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
